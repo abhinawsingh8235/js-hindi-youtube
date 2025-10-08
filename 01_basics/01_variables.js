718bb47a12bf3e2ca4a84abefa4801bc031c29f3
abhinawsingh8235 <abhinawsingh8235@gmail.com>
2025-10-08 04:53:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "abhinaw@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+/*
+    Prefer not to use var
+    because of issue in the block scope and functiona scope
+ */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
