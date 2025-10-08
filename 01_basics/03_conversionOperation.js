dde8444c9956969cfadb0a320121aa26a17dc6e8
abhinawsingh8235 <abhinawsingh8235@gmail.com>
2025-10-08 05:43:17 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "abhinaw" // "33", "33abc", null, undefined, true, "abhinaw"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*  "33" => 33
+    "33abc" => NaN
+    true => 1; false => 0    */
+
+
+let isLoggedIn = 1  //1, 0, "", "abhinaw"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+/*  1 => true;  0 => false
+    "" => false
+    "abhinaw" => true   */
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
