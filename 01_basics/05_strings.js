38788fdf887533a24200faad64d1151e1f95bde6
abhinawsingh8235 <abhinawsingh8235@gmail.com>
2025-10-09 04:25:25 +0000

    Added string, math, and datatype summary files

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "abhinaw"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String('abhinaw hc com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);  
+
+const newStringOne = "   abhinaw   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://abhinaw.com/abhinaw%20singh"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split(' '));
+
